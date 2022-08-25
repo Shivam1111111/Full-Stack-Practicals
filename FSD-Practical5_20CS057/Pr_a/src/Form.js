fd6f49bc0a5d4c868125cf1dde24c9ec08ef9074
Shivam1111111 <97797892+Shivam1111111@users.noreply.github.com>
2022-08-25 12:06:52 +0530

    Add files via upload

---
+++ FSD-Practical5_20CS057/Pr_a/src/Form.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './Form.css';
+import FormSignup from './FormSignup';
+import FormSuccess from './FormSuccess';
+
+const Form = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  function submitForm() {
+    setIsSubmitted(true);
+    alert("Form submitted!");
+  }
+  return (
+    <>
+      <div className='form-container'>
+        <span className='close-btn'>×</span>
+        <div className='form-content-left'>
+          <img className='form-img' src='https://bizgurukul.com/Biz/img/login-form-banner-mob.png' alt='spaceship' />
+        </div>
+        {!isSubmitted ? (
+          <FormSignup submitForm={submitForm} />
+        ) : (
+          <FormSuccess />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Form;
